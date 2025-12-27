6f2813dedc8703fa31ca7012822c965ab7f64327
winnie21042013 <ikpefuawinnie03@gmail.com>
2025-12-27 18:44:12 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#print is used to display words
+print("Hello, this is a sample python scrript.")
+#print always uses parentheses when printing text
+print(3)
+#When its number NEVER use parentheses
+print("i have", 3, "years till i am 18")
+#this is a scentence
+
+import keyword
+print(keyword.kwlist) 
+#identation is very important in python
+
+#this is a var
+age=10
+name="lily"
+
+#you can print vars
+
+print(age)
